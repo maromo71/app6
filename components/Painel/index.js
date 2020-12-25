85ba633b45d5f64653e09d9d72678e86c1a7c2ec
maromo71m <professormoraes@gmail.com>
2020-12-25 11:41:54 -0300

    Instalando o Bootstrap

--- components/Painel/index.js
+++ components/Painel/index.js
@@ -10,9 +10,10 @@ import {
 
 const items = [
   {
-    src: 'https://maromo.com.br/assets/images/bootstrap.png',
-    altText: 'Slide 1',
-    caption: 'Slide 1'
+    src: 'https://raw.githubusercontent.com/maromo71/app6/main/assets/images/bootstrap.png',
+    altText: 'Imagem do Bootstrap',
+    caption: 'Aulas de Bootstrap',
+    
   },
   {
     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
@@ -59,7 +60,7 @@ const Painel = (props) => {
         onExited={() => setAnimating(false)}
         key={item.src}
       >
-        <img src={item.src} alt={item.altText} />
+        <img style={{height: '300px', margin: '0 auto', alignContent: 'center'}} src={item.src} alt={item.altText} />
         <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
       </CarouselItem>
     );
@@ -73,8 +74,8 @@ const Painel = (props) => {
     >
       <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
       {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+      <CarouselControl direction="prev" directionText="Anterior" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Próxima" onClickHandler={next} />
     </Carousel>
   );
 }
