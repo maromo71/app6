affc43c9a737913efccf87574c6ab8f1591dacd5
maromo71m <professormoraes@gmail.com>
2020-12-26 09:23:23 -0300

    Teste Painel

---
+++ pages/etec.js
@@ -0,0 +1,56 @@
+import Link from 'next/link';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Button } from 'reactstrap';
+import Header from '../components/Header';
+
+function Etec() {
+    return <div>
+        <Header />
+        <Container>
+            <h1>Etec</h1>
+            <h3>Etec Pedro Ferreira alves </h3>
+            <div style={{ display: 'block', margin: '0 auto', padding: '10px', width: '18rem' }}>
+                <img style={{ borderRadius: '50%' }} className="card-img-top" alt="Foto do Maromo" src='http://majuconfeccoes.com.br/wp-content/uploads/2019/08/ETEC.png' />
+            </div>
+            <Container>
+            <p>
+                Em 27 de junho de 1892, na cidade de Mogi Mirim, nascia Pedro Ferreira Alves, 
+                segundo filho do Coronel Venâncio Ferreira Alves Adorno. Zizinho, era assim que 
+                Pedro era chamado em sua infância e foi neste momento que encantou-se com 
+                o futebol, um esporte que iniciava-se no Brasil. Em 1910 destacou-se no 
+                Futebol amador ao mesmo tempo que estudava farmácia. Logo depois casou-se 
+                com sua primeira esposa e teve quatro filhos com ela.
+            </p>
+            <p>
+                Em 1919 a habilidade com a bola o destacou entre os atletas do Mogi Mirim Esporte Clube. Popular por ser bom jogador elegeu-se vereador em 1925.  O curioso é que os prefeitos desta época eram eleitos pelos vereadores. Pedro Ferreira Alves é escolhido por seus pares para ser prefeito em seu primeiro mandato no biênio 1926/1927. Em 1927 também perde sua esposa para Febre Tifoide.
+            </p>
+
+            <p>
+                No ano de 1930 funda o Banco da Lavoura de Mogi Mirim, mas seu engajamento político fala mais alto em 1935 vende o banco e transfere-se para capital paulista, permanece por lá até 1943. Volta a Mogi Mirim para seu 2° mandato como prefeito nomeado pelo então governador Fernando Costa. É o momento que inicia-se a construção das grandes galerias de esgoto. Solicita também ao governo estadual a construção de um ginásio industrial.
+            </p>
+
+            <p>
+                Após anos de viuvez em 1945 casa-se novamente com a professora de 1° grau Lucia de Campos Ferreira Alves e no ano seguinte 1946 deixa a prefeitura, mas é nomeado para o cargo de inspetor de menores.
+            </p>
+
+            <p>
+                No ano de 1950 é novamente eleito para vereador para biênio 1951/1952. Com o final do seu mandato de vereador aposenta-se compulsoriamente.
+            </p>
+
+            <p>
+                Vitimado por infarto, falece em 19 de abril de 1962.  Não demora e logo é feita uma homenagem a essa figura ilustríssima da cidade, dá-se o seu nome a escola que passa a ser conhecida por “Ginásio Industrial Pedro Ferreira Alves”, atualmente mais conhecida   por “Etec Pedro Ferreira Alves”.
+            </p>
+
+            <p>
+                Conheça um pouco mais sobre a história da escola clicando aqui.
+            
+            </p>
+            </Container>
+                <Link href="/">
+                        <Button color="primary">Acessar a página home</Button>
+                </Link>
+        </Container>
+   </div>
+}
+
+export default Etec
--- pages/sobre.js
+++ pages/sobre.js
@@ -5,19 +5,31 @@ import Header from '../components/Header';
 
 function Sobre() {
     return <div >
-        <Header/>
+        <Header />
         <Container>
-        <h1>Sobre</h1>
-       <h3>Quem é o Maromo? </h3>
-       <p>
-           Professor da Escola Técnica Estadual Pedro Ferreira Alves, da Faculdade de Tecnologia 
-           de Mogi Mirim e da Faculdade Santa Lúcia de Engenharia da Computação.
+            <h1>Sobre</h1>
+            <h3>Quem é o Maromo? </h3>
+            <div style={{display: 'block', margin: '0 auto', padding: '10px',  width: '18rem'}}>
+                <img  style={{borderRadius: '50%'}} className="card-img-top" alt="Foto do Maromo" src='https://avatars2.githubusercontent.com/u/9035218?s=460&amp;u=589efdc362df4e01493e17a576ea01574f4a2e10&amp;v=4' />
+
+            </div>
+
+            <p>
+                O professor Marcos Roberto de Moraes, <strong>o Maromo</strong>, possui graduação 
+                em Tecnologia Em Processamento de Dados pela Fundação Pinhalense de
+                Ensino (1995), mestrado em Educação pelo Centro Universitário Salesiano São Paulo (2009),
+                especialização em Administração de Sistemas de Informação.
+                Atualmente é professor e coordenador no Curso Superior de Análise e Desenvolvimento de
+                Sistemas do Centro Estadual de Educação Tecnológica Paula Souza (FATEC),
+                professor da Associação Educacional e Assistencial Santa Lúcia no Curso Superior de
+                Engenharia da Computação e professor Nível III do Centro Estadual de Educação Tecnológica
+                Paula Souza (ETEC). Tem experiência na área de Ciência da Computação
        </p>
-       <Link href="/">
-            <Button color="primary">Acessar a página home</Button>
-       </Link>
+            <Link href="/">
+                <Button color="primary">Acessar a página home</Button>
+            </Link>
         </Container>
-       
+
     </div>
 }
 
